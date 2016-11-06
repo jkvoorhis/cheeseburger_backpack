700f2c267a99b249024622b54c24e0a11532a845
jkvoorhis <jhenna.voorhis@gmail.com>
2016-11-06 14:43:46 -0800

    add paging to history requests

--- cheeseburger_backpack/counter.py
+++ cheeseburger_backpack/counter.py
@@ -22,31 +22,50 @@ class Slack_Counter(object):
                 word_list.append(line.rstrip('\n'))
 
         self.word_list = word_list
+        print word_list
 
     def _build_channel_map(self):
         resp = self.slack_client.channels.list(exclude_archived=1)
         channel_list = resp.body['channels']
 
-        for channel in channel_list[:10]:
-            self.channel_map[channel['id']] = channel['name']
+        # # TODO put in way to get channels by name?
+        # for channel in channel_list[:20]:
+        #     self.channel_map[channel['id']] = channel['name']
+
+        self.channel_map['C025Q1VPV'] = 'inane'
 
     def _set_channel_histories(self, weeks=None):
         oldest_timestamp = 0 # slack API's default value. gets entire history.
+        now = datetime.datetime.utcnow()
+        now_timestamp = (now - datetime.datetime(1970, 1, 1)).total_seconds()
+
         if weeks:
-            now =  datetime.datetime.utcnow()
-            oldest_dt = now - datetime.timedelta(weeks=4)
+            oldest_dt = now - datetime.timedelta(weeks=weeks)
             oldest_timestamp = (
                 oldest_dt - datetime.datetime(1970, 1, 1)
             ).total_seconds()
 
         for channel_id in self.channel_map.iterkeys():
             print('getting history for {0}\n'.format(self.channel_map[channel_id]))
-            resp = self.slack_client.channels.history(
-                channel_id,
-                oldest=oldest_timestamp
-            )
-            history = resp.body['messages']
-            messages_text = [message['text'] for message in history]
+
+            messages_text = []
+
+            def get_history(oldest=oldest_timestamp, newest=now_timestamp):
+                print 'getting history for old:{0}, new:{1}'.format(oldest, newest)
+                resp = self.slack_client.channels.history(
+                    channel_id,
+                    oldest=oldest,
+                    latest=newest
+                )
+                history = resp.body['messages']
+                has_more = bool(resp.body['has_more'])
+                for message in history:
+                    messages_text.append(message['text'])
+
+                if has_more:
+                    get_history(newest=history[-1]['ts'])
+
+            get_history()
 
             self.channel_histories.append(
                 {channel_id: messages_text}
@@ -68,6 +87,7 @@ class Slack_Counter(object):
         for channel_dict in self.channel_histories:
             for chan_id, messages in channel_dict.iteritems():
                 cnt = Counter()
+                # print(messages)
                 message_blob = ' '.join(messages)
                 message_list = message_blob.split()
                 for word in message_list:
